1962a8a361e74f0c8b37fe684081e3343f5700f0
Yosary <yl_santosp@unicah.edu>
2022-02-04 20:51:23 -0600

    middleware

--- routes/api/v1/v1.js
+++ routes/api/v1/v1.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const pacientesRoutes = require('./pacientes/pacientes');
 const expedientesRoutes = require('./expedientes/expedientes');
+const { verifyApiHeaderToken } = require('./headerVerifyMiddleware');
 //middlewares
 
-router.use('/pacientes', pacientesRoutes);
+router.use('/pacientes',
+    verifyApiHeaderToken,
+    pacientesRoutes);
 router.use('/expedientes', expedientesRoutes);
 module.exports = router;
